7436ca8a6dcc3151245cdb2440cf89c47bcc5e25
Babs Craig <babscraig@gmail.com>
2018-02-18 15:30:15 +0100

    initialized schema for apollo graphql server

--- imports/startup/server/index.js
+++ imports/startup/server/index.js
@@ -1,1 +1,27 @@
-console.log("Hey hello world!");
+import { createApolloServer } from "meteor/apollo";
+import { makeExecutableSchema } from "graphql-tools";
+
+const typeDefs = `
+type Query {
+  hi: String,
+  myName: String
+}
+`;
+
+const resolvers = {
+  Query: {
+    hi() {
+      return "Hello World! ";
+    },
+    myName() {
+      return "Babasanya Craig";
+    }
+  }
+};
+
+const schema = makeExecutableSchema({
+  typeDefs,
+  resolvers
+});
+
+createApolloServer({ schema });
